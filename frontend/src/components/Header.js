ea315b9baea5cc41510a7d6a174a109cc27b33a7
Andrii Mishtal <mishtal.andriy@gmail.com>
2022-06-20 18:54:07 +0300

    finish productscreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className='header-rigth' id="basic-navbar-nav">
+          <Navbar.Collapse className="header-rigth" id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-              <Nav.Link href="#link"><i className='fas fa-user'></i>Sign In</Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+
+              <Nav.Link as={Link} to="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
